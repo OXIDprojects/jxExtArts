2937a21fc5a60ffb1f7afa3550feeb790022ee3d
Joachim Barthel <jobarthel@gmail.com>
2013-08-05 21:30:27 +0200

    no message

--- copy_this/modules/jxmods/jxextarts/views/admin/en/jxextarts_lang.php
+++ copy_this/modules/jxmods/jxextarts/views/admin/en/jxextarts_lang.php
@@ -5,7 +5,7 @@ $aLang = array(
     'JXEXTARTS_BPRICE'                                     => 'Buying Price',
     'JXEXTARTS_PRICE'                                      => 'Sales Price',
     'JXEXTARTS_TPRICE'                                     => 'SRP',
-    'JXEXTARTS_PICTURES_OPENPREVIEW'                       => 'Opwen',
+    'JXEXTARTS_PICTURES_OPENPREVIEW'                       => 'Open',
     'JXEXTARTS_PICTURES_DELETECONFIRM'                     => 'Do you really want to delete this picture?',
 
     'JXEXTARTS_ERROR_WRONGCHARS'                           => 'EAN/GTIN code contains wrong characters',
